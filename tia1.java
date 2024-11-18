aa3254c045050092f041dae7dd1004054c3a28d6
wesumaluma <wesumaluma@gmail.com>
2024-11-18 13:55:16 +0300

    version 1

---
+++ tia1.java
@@ -0,0 +1,5 @@
+public class tia1{
+public static void main(String a[]){
+        System.out.println("Welcome to the git and github");
+    }
+}
---
+++ tia2.java
@@ -0,0 +1,5 @@
+public class tia2{
+    public static void main (String a[]){
+        System.out.println("Alhamdullilah, for the practical interview");
+    }
+}
